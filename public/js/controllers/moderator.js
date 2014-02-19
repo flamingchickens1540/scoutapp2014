8509262b1390387936400a630b1aa34c0a7ce7ad
lewise <lewise@STU15-150.local>
2014-02-18 16:34:37 -0800

    Added initial moderator interface

--- public/js/controllers/moderator.js
+++ public/js/controllers/moderator.js
@@ -5,5 +5,35 @@ var app = angular.module('ctrl.moderator', [
 ]);
 
 app.controller('ModeratorCtrl', function($scope) {
+
+  $scope.events=[
+    { name:'PNW District Champs', value:'orpo' }
+  ];
+
+/*
+MATCH
+{id: Number,
+moderated: Boolean, //if true, then the match was moderated already
+redAlliance: {team1:Number,team2:Number,team3:Number},
+blueAlliance: {team1:Number,team2:Number,team3:Number},
+}
+  
+
+
+*/
+
+  $scope.matches = [
+    
+    {id: 123,moderated: false, redAlliance: {team1:1540,team2:666,team3:4321}, blueAlliance: {team1:1234,team2:678,team3:8756}},
+    {id: 13,moderated: false, redAlliance: {team1:140,team2:66,team3:432}, blueAlliance: {team1:124,team2:8,team3:876}},
+     
+  ];
+  
+  
+  // array that holds every match at event
+  // select bar (choose event) w/ ng-options
+  // button that ng-click req from server
+  
+
   
 });
