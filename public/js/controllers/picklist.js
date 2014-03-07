4c028bc5f65b2d368fda74230fe208629bdd19e7
Hamzah Khan <hmzh.khn@gmail.com>
2014-03-07 12:32:06 -0800

    Finish new pick list logic
    Need to approve by Peter

--- public/js/controllers/picklist.js
+++ public/js/controllers/picklist.js
@@ -4,118 +4,99 @@ var app = angular.module('ctrl.picklist', [
   'btford.socket-io'
 ]);
 
+
 //This is just some test data at the moment.
 //info and second_picks are both mock data.
 app.controller('PicklistCtrl', function($scope) {
 
-  $scope.allTeams = {
+	var selected = { 'background-color': '#d9534f', 'text-decoration': 'line-through' };
+	var currentSelection = { 'background-color': '#99CC32' }
 
-		1540: {
-		  name: "Flaming Chickens",
-		  selected: false,
-		  styleClass : null,
-		  info: "This is where info for 1540 will go.",
-		  second_picks: [1359, 1432, 2374]
-		},
+	$scope['teams'] = {
+		1540: { name:"The Flaming Chickens", second_picks:[1359, 1432, 2374], 
+			notes:"FRC LOL LOL LOL 1" },
+		1359: { name:"The Scalawags", second_picks:[1359, 1432, 2374], 
+			notes:"FRC LOL LOL LOL 2" },
+		1432: { name:"Mahr's Metal Beavers", second_picks:[1359, 1432, 2374], 
+			notes:"FRC LOL LOL LOL 3" },
+		2374: { name:"Crusaderbots", second_picks:[1359, 1432, 2374], 
+			notes:"FRC LOL LOL LOL 4" },
+		2990: { name:"Hotwire", second_picks:[1359, 1432, 2374], 
+			notes:"FRC LOL LOL LOL 5" },
+		3131: { name:"Gladiators", second_picks:[1359, 1432, 2374], 
+			notes:"FRC LOL LOL LOL 6" },
+		4051: { name:"Sabin-Sharks", second_picks:[1359, 1432, 2374], 
+			notes:"FRC LOL LOL LOL 7" },
+		4127: { name:"Loggerbots", second_picks:[1359, 1432, 2374], 
+			notes:"FRC LOL LOL LOL 8" },
+	};
+	angular.forEach( $scope.teams, function(team, teamNum) {
+		team.selected = false;
+		team.style = null;
+	});
 
-		1359: {
-		  name: "The Scalawags",
-		  selected: false,
-		  styleClass : null,
-		  info: "This is where info for 1359 will go.",
-		  second_picks: [1540, 1432, 2374]
+	//Need input for this part, for now, it's set as an empty object that is later filled with every team.
+  $scope['firstPicks'] = {
+  	1540: { name:"The Flaming Chickens", second_picks:[1359, 1432, 2374] },
+  	1359: { name:"The Scalawags", second_picks:[1359, 1432, 2374] },
+		1432: { name:"Mahr's Metal Beavers", second_picks:[1359, 1432, 2374] },
+  };
+  angular.forEach( $scope.firstPicks, function(team, teamNum) {
+		team.selected = false;
+		team.style = null;
+	});
 
-		},
+  $scope['secondPicks'] = {};
+  $scope['currentTeamInfo'] = {};
 
-		1432: {
-		  name: "Mahr's Metal Beavers",
-		  selected: false,
-		  styleClass : null,
-		  info: "This is where info for 1432 will go.",
-		  second_picks: [1540, 1359, 2374]
-		},
+  $scope.selectTeam = function(teamId) {
 
-		2374:{
-		  name: "Crusaderbots",
-		  selected: false,
-		  styleClass : null,
-		  info: "This is where info for 2374 will go.",
-		  second_picks: [1359, 1432, 1540]
-		},
+		var team = $scope.teams[teamId];
 
-		2990:{
-		  name:"Hotwire",
-		  selected: false,
-		  styleClass : null,
-		  info: "This is where info for 2990 will go.",
-		  second_picks: [1359, 1540, 2374]
-		},
+		// change the styles in teams
+		team.selected = !team.selected;
+		team.style = (team.style != null)? null: selected;
 
-		3131:{
-		  name: "Gladiators",
-		  selected: false,
-		  styleClass : null,
-		  info: "This is where info for 3131 will go.",
-		  second_picks: [1540, 1432, 2990]
-		},
-
-		4051:{
-		  name: "Sabin-Sharks",
-		  selected: false,
-		  styleClass : null,
-		  info: "This is where info for 4051 will go.",
-		  second_picks: [1540, 1432, 3131]
-		},
-
-		4127:{
-		  name: "Loggerbots",
-		  selected: false,
-		  styleClass : null,
-		  info: "This is where info for 4127 will go.",
-		  second_picks: [1359, 3131, 4051]
+		// change the styles in firstPicks
+		if( angular.isDefined($scope.firstPicks[teamId])){
+			$scope.firstPicks[teamId].style = team.style;
+			$scope.firstPicks[teamId].selected = team.selected;
 		}
   };
 
-  //Need input for this part, for now, it's set as an empty object that is later filled with every team.
-  $scope.firstPicks = {};
+  // makes the color green
+  // $scope.toGreenColor = function(teamId) {
+  // 	angular.forEach( $scope.teams, function(team, teamId) {
+  // 		team.style = null;
+  // 	});
 
-  //Need input for this part, until then, it's just a duplication of the allTeams object.
-  $scope.secondPicks = $scope.allTeams;
+  // 	$scope.firstPicks[teamId].style = currentSelection;
+  // };
 
-  $scope.currentTeamInfo = null;
+  $scope.firstPick = {};
 
+  // second teamlist to show up
+  $scope.viewTeamInfo = function(teamId) {
+  	var teamInfo = $scope.teams[teamId];
+		var firstPickInfo = $scope.firstPick = $scope.firstPicks[teamId];
+		$scope['secondPicks'] = {};
+		$scope.firstPick['id'] = teamId;
 
-  $scope.selectTeam = function(teamId) {
-		$scope.allTeams[teamId].selected = !$scope.allTeams[teamId].selected;
-		$scope.allTeams[teamId].styleClass = ($scope.allTeams[teamId].styleClass ==='selected')? null: 'selected';
+		angular.forEach(firstPickInfo.second_picks, function(secondTeamId) {
+			$scope.secondPicks[secondTeamId] = $scope.teams[secondTeamId];
+		});
 
-
-	  angular.forEach($scope.allTeams, function(obj, key) {
-			if (!obj.selected) {
-
-			  $scope.firstPicks[key] = {
-					name: obj.name,
-					selected: obj.selected,
-					styleClass: obj.styleClass,
-					info: obj.info,
-					second_picks: obj.second_picks
-			  };
-			}
-	  });
-
-
-
-	  $scope.viewTeamInfo = function(teamId) {
-			
-			// what does this function do? - Hamzah
-			$scope.currentTeamInfo = teamId;
-	  };
-
-		// run the logic for the first, second lists
+		$scope.currentNotes = teamInfo.notes;
   };
 
-  $scope.getClass = function(inputTeamId) {
+  $scope.getClass = function(teamId) {
+  	console.log($scope.teams[teamId])
+
   	// return the class
-		return $scope.allTeams[inputTeamId].styleClass;
+		return $scope.teams[teamId].style;
+  };
+
+  $scope.deselect = function() {
+
   };
 });
