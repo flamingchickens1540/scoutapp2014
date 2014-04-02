f3d3a30e6d2ede6009fb10b063b0f61d6c19573c
Hamzah Khan <hmzh.khn@gmail.com>
2014-04-01 23:40:58 -0700

    Picklist and mdc changes

--- public/js/controllers/picklist.js
+++ public/js/controllers/picklist.js
@@ -11,15 +11,15 @@ app.controller('PicklistCtrl', function($scope, socket) {
 	var currentSelection = { 'background-color': '#99CC32' }
 
 	// looking for scorers
-	var defensePicks = 				[ 4488, 2471, 1425, 2811, 2374, 2635, 2093, 2990, 3674, 2411, 2521, 4057, 2733, 956, 4132, 4043 ];
-	var lowGoalPicks =  			[ 4488, 2471, 1425, 2811, 2374, 2635, 2093, 2990, 3674, 2411, 2521, 4057, 2733, 956, 4132, 4043 ];
+	var defensePicks = 				[ 4488, 2471, 1425, 2811, 2374, 2635, 2093, 3674, 2411, 2521, 2733, 2990, 4057, 956, 4132, 4043 ];
+	var lowGoalPicks =  			[ 4488, 2471, 1425, 2811, 2374, 2635, 2093, 3674, 2411, 2521, 2733, 2990, 4057, 956, 4132, 4043 ];
 	
 	// looking for passers and defenders
-	var shortOffensePicks = 	[ 4488, 2471, 1425, 2517, 2733, 2811, 3674, 2521, 2626, 2635, 957, 4043, 4051, 4132, 2411, 5085 ];
-	var pureOffensePicks = 		[ 4488, 2471, 1425, 2517, 2733, 2811, 3674, 2521, 2626, 2635, 957, 4043, 4051, 4132, 2411, 5085 ];
+	var shortOffensePicks = 	[ 4488, 2471, 1425, 2517, 2733, 2811, 3674, 2521, 3636, 2635, 957, 4043, 4051, 4132, 2411, 5085 ];
+	var pureOffensePicks = 		[ 4488, 2471, 1425, 2517, 2733, 2811, 3674, 2521, 3636, 2635, 957, 4043, 4051, 4132, 2411, 5085 ];
 	
 	// looking for passer, offensive or defensive
-	var passingPicks = 				[ 4488, 2471, 1425, 2811, 2374, 2635, 2093, 2990, 2411, 2517, 3674, 2733, 4057, 4132, 4043, 956 ];
+	var passingPicks = 				[ 4488, 2471, 1425, 2811, 2374, 2635, 2093, 2411, 2517, 3674, 2733, 2990, 4057, 4132, 4043, 956 ];
 
 	var getEvent = function(eventId) {
     socket.emit('get-event', eventId, function(event) {
@@ -117,13 +117,14 @@ app.controller('PicklistCtrl', function($scope, socket) {
 		});
 
 		$scope.displayInfo = $scope.firstPick.id + ' - ' +  $scope.firstPick.name
-		$scope.currentNotes = teamInfo.masterNotes || 'NO NOTES?';
+		$scope.currentNotes = "MASTER NOTES: "+teamInfo.masterNotes || 'NO NOTES?';
   };
 
   $scope.secondPick = {};
 
   $scope.viewSecondTeamInfo = function(teamId) {
 
+  	var team = $scope.teams[teamId];
   	$scope.currentNotes = teamId;
 
   	var teamInfo = $scope.teams[teamId];
@@ -131,7 +132,68 @@ app.controller('PicklistCtrl', function($scope, socket) {
 
 		//$scope.secondPick['id'] = teamId;
 		$scope.displayInfo = $scope.secondPick.id + ' - ' +  $scope.secondPick.name
-		$scope.currentNotes = $scope.secondPick.masterNotes || 'NO NOTES?';
+		$scope.currentNotes = "MASTER NOTES: "+($scope.secondPick.masterNotes || 'NO NOTES?');
+
+		var matches = teamInfo.matches || [];
+
+		var scoutData = {
+			high: 0,
+			highTotal: 0,
+			highRatio: 0.0,
+
+			low: 0,
+			lowTotal: 0,
+			lowRatio: 0.0,
+
+			autoBalls: 0,
+			autoHotgoals: 0,
+			autoMisses: 0,
+
+			pass: 0,
+			passTotal: 0,
+			passRatio: 0.0,
+
+			trussPasses: 0,
+			totalTrussPasses: 0,
+			trussRatio: 0.0
+		};
+
+		matches.forEach( function(teamMatch) {
+			var data = teamMatch.data;
+
+			scoutData.autoBalls += data.auto.fieldValues.goal;
+			scoutData.autoHotgoals += data.auto.fieldValues.hotgoal;
+			scoutData.autoMisses += data.auto.fieldValues.miss;
+
+			scoutData.highTotal += (data.scoring.goals.high + data.scoring.goals.highMisses);
+			scoutData.high += data.scoring.goals.high;
+
+			scoutData.lowTotal += (data.scoring.goals.low + data.scoring.goals.lowMisses);
+			scoutData.low += data.scoring.goals.low;
+
+			scoutData.pass += data.teamwork.passing.roll + data.teamwork.passing.aerial //+ data.teamwork.passing.aerialMisses + data.teamwork.passing.rollMisses;
+			scoutData.passTotal += data.teamwork.passing.roll + data.teamwork.passing.aerial;
+
+			scoutData.trussPasses += data.teamwork.passing.truss;
+			scoutData.totalTrussPasses += data.teamwork.passing.truss; //+ data.teamwork.passing.trussMisses;
+		});
+
+		scoutData.lowRatio = scoutData.low +'/'+ scoutData.lowTotal;
+		scoutData.highRatio = scoutData.high +'/'+ scoutData.highTotal;
+		scoutData.passRatio = scoutData.pass+'/'+scoutData.passTotal;
+		scoutData.trussRatio = scoutData.trussPasses+'/'+scoutData.totalTrussPasses;
+
+		$scope.moreData = "Play Style: "+ ((((team.pit || {}).robot || {}).playstyle) || 'No pit data for Play Style')+'\n'+
+			"Shifting: "+ ((((team.pit || {}).general || {}).shifting) || 'No shifting data found')+'\n'+
+			//"Wheels: "+ wheelsText.join(',')+'\n'+
+			//'Shooting Range: '+ rangeText.join(',')+'\n'+
+			"High: " + scoutData.highRatio+'\n'+
+			"Low: "+ scoutData.lowRatio+'\n'+
+			"Passes: " + scoutData.passRatio+'\n'+
+			"Truss Passes: " + scoutData.trussRatio+'\n'+
+			"Auto Goals (including hot): " + scoutData.autoBalls+'/'+(scoutData.autoBalls + scoutData.autoMisses)+'\n'+
+			"Hot goals out of all goals made: " + scoutData.autoHotgoals+'/'+scoutData.autoBalls +'\n';
+
   };
 
   $scope.getClass = function(teamId) {
